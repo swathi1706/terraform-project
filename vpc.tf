df2ffb73675c76a6369d333c3968c1c850ba75dd
yourmailid <yourmailid>
2025-07-24 17:07:25 +0000

    latest

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 # Internet VPC
 resource "aws_vpc" "main" {
-  cidr_block           = "12.0.0.0/16"
+  cidr_block           = "11.0.0.0/16"
   instance_tenancy     = "default"
   enable_dns_support   = "true"
   enable_dns_hostnames = "true"
@@ -12,7 +12,7 @@ resource "aws_vpc" "main" {
 # Subnets
 resource "aws_subnet" "main-public-1" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "12.0.1.0/24"
+  cidr_block              = "11.0.1.0/24"
   map_public_ip_on_launch = "true"
   availability_zone       = "ap-south-1a"
 
@@ -23,7 +23,7 @@ resource "aws_subnet" "main-public-1" {
 
 resource "aws_subnet" "main-public-2" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "12.0.2.0/24"
+  cidr_block              = "11.0.2.0/24"
   map_public_ip_on_launch = "true"
   availability_zone       = "ap-south-1b"
 
@@ -34,7 +34,7 @@ resource "aws_subnet" "main-public-2" {
 
 resource "aws_subnet" "main-public-3" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "12.0.3.0/24"
+  cidr_block              = "11.0.3.0/24"
   map_public_ip_on_launch = "true"
   availability_zone       = "ap-south-1c"
 
@@ -45,7 +45,7 @@ resource "aws_subnet" "main-public-3" {
 
 resource "aws_subnet" "main-private-1" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "12.0.4.0/24"
+  cidr_block              = "11.0.4.0/24"
   map_public_ip_on_launch = "false"
   availability_zone       = "ap-south-1a"
 
@@ -56,7 +56,7 @@ resource "aws_subnet" "main-private-1" {
 
 resource "aws_subnet" "main-private-2" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "12.0.5.0/24"
+  cidr_block              = "11.0.5.0/24"
   map_public_ip_on_launch = "false"
   availability_zone       = "ap-south-1b"
 
@@ -67,7 +67,7 @@ resource "aws_subnet" "main-private-2" {
 
 resource "aws_subnet" "main-private-3" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "12.0.6.0/24"
+  cidr_block              = "11.0.6.0/24"
   map_public_ip_on_launch = "false"
   availability_zone       = "ap-south-1c"
 
